316e848800cff50cc6c005cb14ff2f5fdf40c2a4
Maycon Queiroz <fmaycon@gmail.com>
2022-10-23 12:25:42 -0300

    add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createCourse } from './routes'
 
 
 const app = express()
 
-app.get('/', createCourse)
+
+app.get('/', (request, response) => response.json({ error: "ahhh Maycon" }))
+
 
 app.listen(3333)
