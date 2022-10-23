e9bfcd22235b13686dc340075202f2028448c8be
Maycon Queiroz <fmaycon@gmail.com>
2022-10-23 12:49:56 -0300

    build(debug): add config to debug

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import express from 'express'
 
 
 const app = express()
+app.use(express.json())
 
 
 app.get('/', (request, response) => response.json({ error: "ahhh Maycon" }))
 
+app.post('/', (request, response) => {
+  const { name } = request.body
+
+  response.json({ name })
+})
 
 app.listen(3333)
