ee70c84b2db025b247e2fb1808a2cbff15f88cfc
Maycon Queiroz <fmaycon@gmail.com>
2022-10-24 23:12:53 -0300

    feat(Specification): create repository and interface to Specification

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,39 @@
+import { ICreateSpecificationDTO, ISpecificationRepository } from './ISpecificationRepository';
+import { Specification } from '../model/Specification';
+
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[]
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+
+  findByName(name: string): Specification | undefined {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
+
+  list() {
+    return this.specifications;
+  }
+
+
+}
+
+export { SpecificationRepository }
