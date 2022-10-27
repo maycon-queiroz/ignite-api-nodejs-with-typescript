ccd808ea34f6208ab811a77af5a0e94d02093c02
Maycon Queiroz <fmaycon@gmail.com>
2022-10-27 00:02:16 -0300

    feat(Import): add loadCategories to read categories

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,32 @@
 import { parse } from 'csv-parse';
 import fs from 'fs';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    console.log(file);
-    const stream = fs.createReadStream(file.path);
 
-    const parseFile = parse();
+  async loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = []
 
-    stream.pipe(parseFile);
+      const parseFile = parse();
 
-    parseFile.on("data", async (line) => {
-      console.log(line)
+      stream.pipe(parseFile);
+
+      parseFile.on("data", async (line) => {
+        const [name, description] = line;
+        categories.push({ name, description })
+      })
+        .on("end", () => resolve(categories))
+        .on("error", (error) => reject(error));
     })
+  }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
