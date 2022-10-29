4a32204acd8428bd21459f690b3703cc4cba3ef7
Maycon Queiroz <fmaycon@gmail.com>
2022-10-29 13:55:31 -0300

    feat(swagger): add swagger config and implement in server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express"
+
 import { router } from './routes';
+import swaggerFiler from "../swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerFiler));
 
 app.use(router);
 
