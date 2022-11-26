6418eee6b2bec25fa6da0313cf0cd8068a426745
Maycon Queiroz <fmaycon@gmail.com>
2022-11-25 22:23:48 -0300

    fix(database): change host

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "database",
     port: 5432,
     username: "docker",
     password: "ignite",
